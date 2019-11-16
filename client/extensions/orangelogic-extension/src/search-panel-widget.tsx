bbf4b539fc5256304cc30dfb71a03dff4b8d2b5d
Petr Jasek <jasekpetr@gmail.com>
2019-11-16 09:03:52 +0100

    initial search panel extension

---
+++ client/extensions/orangelogic-extension/src/search-panel-widget.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import {ISearchPanelWidgetProps, ISuperdesk} from 'superdesk-api';
+
+export const searchPanelWidgetFactory = (gettext: ISuperdesk['localization']['gettext']) => {
+    const mediaTypes = [
+        {
+            type: 'Image',
+            label: gettext('Picture'),
+        },
+        {
+            type: 'Video',
+            label: gettext('Video'),
+        },
+    ];
+
+    return class SearchPanelWidget extends React.PureComponent<ISearchPanelWidgetProps> {
+
+        toggleMediaType(type: string) {
+            const mediaTypes = this.props.params.mediaTypes || {};
+
+            mediaTypes[type] = !mediaTypes[type];
+            this.props.setParams({mediaTypes});
+        }
+
+        isActive(type: string) {
+            return this.props.params.mediaTypes != null && this.props.params.mediaTypes[type] === true;
+        }
+
+        render() {
+            const {params} = this.props;
+            return (
+                <fieldset>
+                    <div className="field">
+                        <label className="search-label"></label>
+                        {mediaTypes.map((type) => (
+                            <button key={type.type}
+                                className={'btn btn--primary' + (this.isActive(type.type) ? ' btn--active' : '')}
+                                onClick={() => this.toggleMediaType(type.type)}
+                            >{type.label}</button>
+                        ))}
+                    </div>
+                    <div className="field">
+                        <label className="search-label">{gettext('From')}</label>
+                        <input type="date" value={params.from || ''}
+                            onChange={(event) => this.props.setParams({from: event.target.value})}
+                        />
+                    </div>
+                    <div className="field">
+                        <label className="search-label">{gettext('To')}</label>
+                        <input type="date" value={params.to || ''}
+                            onChange={(event) => this.props.setParams({to: event.target.value})}
+                        />
+                    </div>
+                </fieldset>
+            );
+        }
+    };
+};
--- client/index.js
+++ client/index.js
@@ -1,10 +1,9 @@
 import {startApp} from 'superdesk-core/scripts/index';
-import annotationsLibraryExtension from 'superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension';
-import markForUserExtension from 'superdesk-core/scripts/extensions/markForUser/dist/src/extension';
+import orangelogicExtension from 'orangelogic-extension';
 
 setTimeout(() => {
     startApp(
-        [annotationsLibraryExtension, markForUserExtension],
+        [orangelogicExtension],
         {},
     );
 });
