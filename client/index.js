1ca58e4bc77108b5bf667b9996b05dc744f61d1d
Petr Jasek <jasekpetr@gmail.com>
2020-02-21 16:04:42 +0100

    enable planning
    IN-4085

--- client/index.js
+++ client/index.js
@@ -1,9 +1,13 @@
 import {startApp} from 'superdesk-core/scripts/index';
+import planningExtension from 'superdesk-planning-extension';
 import orangelogicExtension from './extensions/orangelogic-extension/dist/extension';
 
 setTimeout(() => {
     startApp(
-        [orangelogicExtension],
+        [
+            planningExtension,
+            orangelogicExtension,
+        ],
         {},
     );
 });
