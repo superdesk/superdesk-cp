be21dd403b5d0c9304d631cf4d51819bd73bb048
Mark Pittaway <mark.pittaway@gmail.com>
2020-04-28 18:26:45 +1000

    Use develop branch for analytics and planning

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 import {startApp} from 'superdesk-core/scripts/index';
-import planningExtension from 'superdesk-planning-extension';
+import planningExtension from 'superdesk-planning/client/planning-extension/dist/extension';
 import orangelogicExtension from './extensions/orangelogic-extension/dist/extension';
 
 setTimeout(() => {
