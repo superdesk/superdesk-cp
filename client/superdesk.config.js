dd1657d54b5c5c755a289ad6812311ea42dd4d6a
Karel Petrak <karel.petrak@sourcefabric.org>
2020-07-21 10:23:49 +0200

    Update Search CVs

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -101,6 +101,7 @@ module.exports = function(grunt) {
         },
         
         search_cvs: [
+            {id: 'categories', name:'Categories', field: 'anpa_category', list: 'categories'},
             {id: 'subject_custom', name:'Index', field: 'subject', list: 'subject_custom'},
             {id: 'distribution', name:'Services', field: 'subject', list: 'distribution'},
             {id: 'languages', name:'Language', field: 'subject', list: 'languages'}
