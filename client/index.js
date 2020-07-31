89a8c5a1136be5ba753fb65b739cd251f5d1c0d3
Petr Jasek <jasekpetr@gmail.com>
2020-07-31 12:55:19 +0200

    enable mark for user extension

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import {startApp} from 'superdesk-core/scripts/index';
 import planningExtension from 'superdesk-planning/client/planning-extension/dist/extension';
 import orangelogicExtension from './extensions/orangelogic-extension/dist/extension';
 import uploadIptc from './extensions/upload-iptc';
+import markForUserExtension from 'superdesk-core/scripts/extensions/markForUser';
 
 setTimeout(() => {
     startApp(
@@ -9,6 +10,7 @@ setTimeout(() => {
             planningExtension,
             orangelogicExtension,
             uploadIptc,
+            markForUserExtension,
         ],
         {},
     );
