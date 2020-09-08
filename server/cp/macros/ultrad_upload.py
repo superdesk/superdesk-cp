1b9d8df3977b8fe64ac4f4659b830080c374f0a2
Petr Jasek <jasekpetr@gmail.com>
2020-09-08 11:31:18 +0200

    implement ultrad integration
    there is a macro which will upload item to ultrad
    and then a celery task which check once a minute
    if any of those stories is ready to be exported and
    sync item in superdesk if so.
    
    SDCP-140

---
+++ server/cp/macros/ultrad_upload.py
@@ -0,0 +1,29 @@
+
+from flask_babel import lazy_gettext
+from flask import current_app as app
+
+from cp.ultrad import upload_document, ULTRAD_ID
+
+
+def callback(item, **kwargs):
+    item.setdefault('extra', {})
+    if item['extra'].get(ULTRAD_ID):
+        app.logger.info('item %s is already in ultrad', item['guid'])
+        return item
+
+    if not item.get('body_html'):
+        app.logger.debug('nothing to translate for item %s', item['guid'])
+        return item
+
+    # make it fail
+    ultrad_id = upload_document(item)
+    item['extra'][ULTRAD_ID] = ultrad_id
+
+    return item
+
+
+name = 'ultrad-upload'
+label = lazy_gettext('Upload to Ultrad')
+group = lazy_gettext('translate')
+access_type = 'frontend'
+action_type = 'direct'
