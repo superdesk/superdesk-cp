e8dc84ff31d7588e96bdfc3189a1c3705c239203
Petr Jasek <jasekpetr@gmail.com>
2021-01-11 11:28:40 +0100

    set new default value for orangelogic

--- server/settings.py
+++ server/settings.py
@@ -330,6 +330,6 @@ AP_TAGS_MAPPING = {
 
 SCHEMA_VERSION = 2
 
-ORANGELOGIC_URL = env('ORANGELOGIC_URL', '')
+ORANGELOGIC_URL = env('ORANGELOGIC_URL', 'https://canadianpress-api.orangelogic.com/')
 WORKFLOW_ALLOW_MULTIPLE_UPDATES = True
 UPDATE_TRANSLATION_METADATA_MACRO = 'Update translation metadata macro'
