b50a9a9d60c25122bc1aecb88567ee3741a8f283
GyanP <gyanig72@gmail.com>
2021-02-16 19:15:02 +0530

    Merge pull request #112 from GyanP/SDESK-5813
    translation macro (SDCP-392) always overwrite existing destinations with the french version. [SDESK-5813]

--- server/cp/macros/update_translation_metadata_macro.py
+++ server/cp/macros/update_translation_metadata_macro.py
@@ -14,6 +14,14 @@ from superdesk import get_resource_service
 
 logger = logging.getLogger(__name__)
 
+destination_qcodes = ["sfstf", "apfra", "cpstf", "ap---"]
+
+
+def get_destination(items, qcode):
+    for item in items:
+        if item.get("qcode", "") == qcode:
+            return item
+
 
 def update_translation_metadata_macro(item, **kwargs):
     if item.get("anpa_take_key"):
@@ -26,21 +34,55 @@ def update_translation_metadata_macro(item, **kwargs):
     if not cv or not cv.get("items"):
         return
 
-    subject = item.get("subject", [])
-    is_destination = any(
-        sub for sub in subject if sub.get("name") == "Presse Canadienne staff"
-    )
+    subjects = item.get("subject", [])
 
-    for value in cv["items"]:
-        if value.get("name") == "Presse Canadienne staff" and not is_destination:
-            item.setdefault("subject", []).append(
-                {
-                    "name": value["name"],
-                    "qcode": value["qcode"],
-                    "scheme": "destinations",
-                }
+    # Remove out all the fields which are of no use.
+    subjects = [
+        sub
+        for sub in subjects
+        if (
+            (
+                sub.get("qcode") in destination_qcodes
+                and sub.get("scheme") == "destinations"
             )
-            break
+            or sub.get("scheme") != "destinations"
+        )
+    ]
+
+    destinations = [
+        cv_item for cv_item in cv["items"] if cv_item.get("qcode") in destination_qcodes
+    ]
+
+    # If subjects are present override them else add a new subject
+    if subjects:
+        for subject in subjects:
+            destination = {}
+            if subject.get("qcode") == "cpstf":
+                destination = get_destination(destinations, "sfstf")
+
+            elif subject.get("qcode") == "ap---":
+                destination = get_destination(destinations, "apfra")
+
+            if destination:
+                subject.update(
+                    {
+                        "name": destination.get("name"),
+                        "qcode": destination.get("qcode"),
+                        "scheme": "destinations",
+                    }
+                )
+    else:
+        destination = get_destination(destinations, "sfstf")
+
+        subjects = [
+            {
+                "name": destination["name"],
+                "qcode": destination["qcode"],
+                "scheme": "destinations",
+            }
+        ]
+
+    item["subject"] = subjects
 
     return item
 
