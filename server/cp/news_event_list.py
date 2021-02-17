f32397f4bc152d51a1e2e61f2d5c80cb0bc5af0e
MarkLark86 <mark.pittaway@gmail.com>
2021-02-17 17:07:34 +1100

    [SDCP-476] Add coverage information to News Events List template (#114)
    Also show:
    * local times for each event
    * long definition
    * website links

--- server/cp/news_event_list.py
+++ server/cp/news_event_list.py
@@ -1,3 +1,5 @@
+from superdesk.utc import utc_to_local
+
 STATE_GROUPS = {
     "atlantic": [
         "newfoundland and labrador",
@@ -19,14 +21,28 @@ def get_group_items(items, state_group):
     group_items = []
 
     for item in items:
-        if item.get("location"):
-            country = item["location"][0]["address"]["country"].lower()
-            locality = item["location"][0]["address"]["locality"].lower()
+        tz = item['dates']['tz']
+        item['dates']['start_local'] = utc_to_local(tz, item['dates']['start'])
+        item['dates']['end_local'] = utc_to_local(tz, item['dates']['end'])
+        item['dates']['tz_name'] = item['dates']['end_local'].tzname()
 
-            if country != "canada" and state_group == "miscellaneous":
-                group_items.append(item)
-            elif country == "canada" and locality in STATE_GROUPS[state_group]:
-                group_items.append(item)
+        if item.get("location"):
+            try:
+                country = item["location"][0]["address"]["country"].lower()
+                locality = item["location"][0]["address"]["locality"].lower()
+
+                if country != "canada" and state_group == "miscellaneous":
+                    group_items.append(item)
+                elif country == "canada" and locality in STATE_GROUPS[state_group]:
+                    group_items.append(item)
+                elif state_group == "miscellaneous":
+                    group_items.append(item)
+            except (IndexError, KeyError):
+                if state_group == "miscellaneous":
+                    group_items.append(item)
+        elif state_group == "miscellaneous":
+            group_items.append(item)
+
     return sorted(group_items, key=lambda group_item: group_item["dates"]["start"])
 
 
