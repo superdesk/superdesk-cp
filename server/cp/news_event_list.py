5be530973db7279ce7bd3a0349b0a6b9d1dcba75
MarkLark86 <mark.pittaway@gmail.com>
2021-02-26 18:22:08 +1100

    [SDCP-484] Support Cominbed and Planning filters for 'News Events List' template (#118)

--- server/cp/news_event_list.py
+++ server/cp/news_event_list.py
@@ -1,3 +1,4 @@
+from flask import current_app as app
 from superdesk.utc import utc_to_local
 
 STATE_GROUPS = {
@@ -13,35 +14,88 @@ STATE_GROUPS = {
     "prairies": ["manitoba", "saskatchewan", "alberta"],
     "british columbia": ["british columbia"],
     "north": ["nunavut", "northwest territories", "yukon"],
-    "miscellaneous": [],
+    "undated": [],
 }
+
+
+def set_item_details(items):
+    for item in items:
+        # Prioritise the Event's slugline/name before Planning item's
+        item["title"] = item.get("slugline") or \
+            item.get("name") or \
+            ''
+
+        # Prioritise the Event's description before Planning item's
+        event = item.get("event") or item
+        item["description"] = event.get("definition_long") or \
+            event.get("definition_short") or \
+            item.get("description_text") or \
+            ''
+
+        if item["type"] == "planning":
+            # Use the Event dates if available
+            # otherwise fall back to the Planning date
+            if (item.get("event") or {}).get("dates"):
+                item["dates"] = item["event"]["dates"]
+
+                # Attach Events location/links for sorting/displaying
+                item["location"] = item["event"].get("location")
+                item["links"] = item["event"].get("links")
+            else:
+                item["dates"] = {
+                    "start": item["planning_date"],
+                    "tz": app.config["DEFAULT_TIMEZONE"]
+                }
+
+        if len(item.get("location") or []):
+            # Set the items Location details if available
+            try:
+                item["country"] = item["location"][0]["address"]["country"].lower()
+                item["locality"] = item["location"][0]["address"]["locality"].lower()
+            except (IndexError, KeyError):
+                pass
+
+        # Construct the date string here so we don't have to use
+        # {% if %} {% else %} statements in the template
+        tz = item["dates"]["tz"]
+        start_local = utc_to_local(tz, item["dates"]["start"])
+        start_local_str = start_local.strftime('%I:%M %P')
+        end_local = utc_to_local(tz, item["dates"]["end"]) if item["dates"].get("end") else None
+        end_local_str = end_local.strftime('%I:%M %P') if end_local else None
+        tz_name = start_local.tzname()
+
+        if end_local:
+            item["local_date"] = f'{start_local_str} - {end_local_str} ({tz_name})'
+        else:
+            item["local_date"] = f'{start_local_str} ({tz_name})'
 
 
 def get_group_items(items, state_group):
     group_items = []
 
     for item in items:
-        tz = item['dates']['tz']
-        item['dates']['start_local'] = utc_to_local(tz, item['dates']['start'])
-        item['dates']['end_local'] = utc_to_local(tz, item['dates']['end'])
-        item['dates']['tz_name'] = item['dates']['end_local'].tzname()
+        if item.get("_added_to_group"):
+            # if this item has already been added to a group
+            # then don't try and process it again
+            continue
 
-        if item.get("location"):
-            try:
-                country = item["location"][0]["address"]["country"].lower()
-                locality = item["location"][0]["address"]["locality"].lower()
+        add_to_group = False
+        if item.get("country") and item.get("locality"):
+            country = item["country"]
+            locality = item["locality"]
 
-                if country != "canada" and state_group == "miscellaneous":
-                    group_items.append(item)
-                elif country == "canada" and locality in STATE_GROUPS[state_group]:
-                    group_items.append(item)
-                elif state_group == "miscellaneous":
-                    group_items.append(item)
-            except (IndexError, KeyError):
-                if state_group == "miscellaneous":
-                    group_items.append(item)
-        elif state_group == "miscellaneous":
+            if country != "canada" and state_group == "undated":
+                add_to_group = True
+            elif country == "canada" and locality in STATE_GROUPS[state_group]:
+                add_to_group = True
+            elif state_group == "undated":
+                add_to_group = True
+        elif state_group == "undated":
+            add_to_group = True
+
+        if add_to_group:
             group_items.append(item)
+            item["_added_to_group"] = True
 
     return sorted(group_items, key=lambda group_item: group_item["dates"]["start"])
 
@@ -49,10 +103,11 @@ def get_group_items(items, state_group):
 def group_items_by_state(items):
     groups = {}
 
+    set_item_details(items)
     for state_group in STATE_GROUPS:
         groups[state_group] = {
             "name": state_group,
             "_items": get_group_items(items, state_group),
         }
 
-    return groups
+    return groups.items()
