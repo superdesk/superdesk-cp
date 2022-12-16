1c4f43ac0e172775055438ec00e67fdc92598933
Petr Jašek <jasekpetr@gmail.com>
2022-12-16 06:03:54 -0500

    set province based on dateline when publishing (#174)
    so that we can setup content filters based on provinces
    
    SDCP-647

---
+++ server/cp/set_province_on_publish.py
@@ -0,0 +1,30 @@
+import superdesk
+
+from superdesk.signals import item_publish
+
+PROVINCE_CV = "regions"
+
+
+def set_province_on_publish(sender, item, updates, **kwargs):
+    try:
+        region = item["dateline"]["located"]["state"]
+    except (AttributeError, KeyError):
+        return
+    if not region:
+        return
+    updates.setdefault("subject", item.get("subject") or [])
+    for subj in updates["subject"]:
+        if subj.get("scheme") == PROVINCE_CV:
+            return
+    provinces = superdesk.get_resource_service("vocabularies").get_items(
+        PROVINCE_CV, is_active=True
+    )
+    for province in provinces:
+        if province.get("name", "").lower() == region.lower():
+            updates["subject"].append(province)
+            item["subject"] = updates["subject"]
+            return
+
+
+def init_app(app):
+    item_publish.connect(set_province_on_publish)
