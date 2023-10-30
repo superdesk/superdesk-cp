a6d2c32e7520806d4261bcf7deb364d3c7809c42
Mark Pittaway <mark.pittaway@gmail.com>
2023-10-30 17:04:27 +1100

    [CPCN-27] CP Transcripts ingest parser

---
+++ server/cp/ingest/parser/cp_transcripts.py
@@ -0,0 +1,40 @@
+from typing import Dict, Any, Optional
+
+from superdesk import get_resource_service
+from superdesk.io.feed_parsers.ninjs import NINJSFeedParser
+from superdesk.text_utils import plain_text_to_html
+
+
+def get_previous_version(original_ingest_id: str, version_number: int) -> Optional[Dict[str, Any]]:
+    while version_number > 0:
+        ingest_id = f"{original_ingest_id}.{version_number}"
+        prev_item = get_resource_service("archive").find_one(req=None, ingest_id=ingest_id)
+
+        if prev_item is not None:
+            return prev_item
+        version_number -= 1
+
+    return None
+
+
+class CPTranscriptsFeedParser(NINJSFeedParser):
+    NAME = "cp_transcripts"
+    label = "CP Transcripts"
+
+    def _transform_from_ninjs(self, ninjs: Dict[str, Any]):
+        original_guid = ninjs["guid"]
+        version = int(ninjs["version"])
+        ninjs["guid"] = f"{original_guid}.{version}"
+        item = super()._transform_from_ninjs(ninjs)
+        item["version"] = version
+        item["body_html"] = plain_text_to_html(item["body_html"])
+        item.setdefault("extra", {}).update(dict(
+            publish_ingest_id_as_guid=True,
+            cp_version=version,
+            type="transcript",
+        ))
+
+        previous_item = get_previous_version(original_guid, version - 1)
+        if previous_item is not None:
+            item["rewrite_of"] = previous_item["ingest_id"]
+        return item
---
+++ server/tests/ingest/parser/cp_transcripts.py
@@ -0,0 +1,36 @@
+import unittest
+from unittest.mock import patch
+
+import flask
+import superdesk
+
+from cp.ingest import CPTranscriptsFeedParser
+
+from tests.ingest.parser import get_fixture_path
+from tests.mock import resources
+
+
+provider = {}
+parser = CPTranscriptsFeedParser()
+
+
+class CP_Transcripts_ParseTestCase(unittest.TestCase):
+    app = flask.Flask(__name__)
+
+    def test_parse(self):
+        with self.app.app_context(), patch.dict(superdesk.resources, resources):
+            superdesk.resources["archive"].service.find_one.side_effect = [
+                {"ingest_id": "d3c8487a-1757-4dde-8bb5-22ca166c1e67.1", "version": 2, "extra": {"ap_version": 999}},
+            ]
+            items = parser.parse(get_fixture_path("cp_transcripts.json", "cp_transcripts"), provider)
+            superdesk.resources["archive"].service.find_one.side_effect = None
+
+        item = items[0]
+        self.assertEqual("text", item["type"])
+        self.assertEqual("transcript", item["extra"]["type"])
+        self.assertEqual(True, item["extra"]["publish_ingest_id_as_guid"])
+        self.assertEqual(2, item["extra"]["cp_version"])
+        self.assertEqual("d3c8487a-1757-4dde-8bb5-22ca166c1e67.2", item["guid"])
+        self.assertEqual(2, item["version"])
+        self.assertEqual("d3c8487a-1757-4dde-8bb5-22ca166c1e67.1", item["rewrite_of"])
+        self.assertTrue(item["body_html"].startswith("<p>laying around"))
