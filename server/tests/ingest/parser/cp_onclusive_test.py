b486b606e47a11cc7bec62540395519db02fcfb4
devketanpro <devketanpro11@gmail.com>
2024-04-11 23:40:20 +0530

    add testcases

--- server/tests/ingest/parser/cp_onclusive_test.py
+++ server/tests/ingest/parser/cp_onclusive_test.py
@@ -6,7 +6,6 @@ from . import ParserTestCase
 from superdesk.metadata.item import (
     ITEM_TYPE,
     CONTENT_TYPE,
-    GUID_FIELD,
     CONTENT_STATE,
 )
 import flask
@@ -14,6 +13,10 @@ import superdesk
 from tests.ingest.parser import get_fixture_path
 from tests.mock import resources
 from unittest.mock import patch
+from superdesk import get_resource_service
+from superdesk.io.commands.update_ingest import ingest_item
+from superdesk.tests import TestCase as _TestCase, update_config, setup
+from superdesk.factory.app import get_app
 
 
 with open(get_fixture_path("cp_onclusive.json", "cp_onclusive")) as fp:
@@ -24,7 +27,7 @@ def qcode(subject):
     return "{}:{}".format(subject.get("scheme"), subject.get("qcode"))
 
 
-class OnclusiveFeedParserTestCase(ParserTestCase):
+class OnclusiveFeedParserTestCase(_TestCase):
     parser = CPOnclusiveFeedParser()
     provider = "Test_CP_Onclusive"
     app = flask.Flask(__name__)
@@ -32,144 +35,180 @@ class OnclusiveFeedParserTestCase(ParserTestCase):
     maxDiff = None
 
     def test_content(self):
-        with self.app.app_context():
-            with patch.dict(superdesk.resources, resources):
-                item = self.parser.parse(data)[0]
-                expected_subjects = [
-                    {
-                        "name": "Conflict / Terrorism / Security",
-                        "qcode": "133",
-                        "scheme": "onclusive_categories",
+        with patch.dict(superdesk.resources, resources):
+            item = self.parser.parse(data)[0]
+            expected_subjects = [
+                {
+                    "name": "Conflict / Terrorism / Security",
+                    "qcode": "133",
+                    "scheme": "onclusive_categories",
+                },
+                {
+                    "name": "UK Regional Elections",
+                    "qcode": "148",
+                    "scheme": "onclusive_event_types",
+                },
+                {
+                    "name": "Testing",
+                    "qcode": "149",
+                    "scheme": "onclusive_event_types",
+                },
+                {
+                    "name": "Banking",
+                    "qcode": "159",
+                    "scheme": "onclusive_categories",
+                },
+                {
+                    "name": "Music Festivals",
+                    "qcode": "228",
+                    "scheme": "onclusive_event_types",
+                },
+                {
+                    "name": "Testing2",
+                    "qcode": "129",
+                    "scheme": "onclusive_event_types",
+                },
+                {
+                    "name": "Finance General",
+                    "qcode": "35",
+                    "scheme": "onclusive_categories",
+                },
+                {
+                    "name": "Tech - Internet, software & new media",
+                    "qcode": "50",
+                    "scheme": "onclusive_categories",
+                },
+                {
+                    "name": "Law & Order",
+                    "qcode": "88",
+                    "scheme": "onclusive_categories",
+                },
+                {
+                    "name": "Trade Conferences",
+                    "qcode": "97",
+                    "scheme": "onclusive_categories",
+                },
+                {
+                    "qcode": "Conference",
+                    "scheme": "event_types",
+                },
+                {
+                    "qcode": "Conference and trade show",
+                    "scheme": "event_types",
+                },
+                {
+                    "qcode": "Official visit",
+                    "scheme": "event_types",
+                },
+                {
+                    "qcode": "04000000",
+                    "scheme": "subject_custom",
+                },
+                {
+                    "qcode": "20000638",
+                    "scheme": "subject_custom",
+                },
+                {
+                    "qcode": "11000000",
+                    "scheme": "subject_custom",
+                },
+            ]
+            self.assertEqual(
+                sorted(map(qcode, item["subject"])),
+                sorted(map(qcode, expected_subjects)),
+            )
+            item["anpa_category"].sort(key=lambda i: i["name"])
+            expected_categories = [
+                {
+                    "name": "National",
+                    "qcode": "g",
+                    "translations": {
+                        "name": {
+                            "en-CA": "National",
+                            "fr-CA": "Nouvelles Générales",
+                        }
                     },
-                    {
-                        "name": "UK Regional Elections",
-                        "qcode": "148",
-                        "scheme": "onclusive_event_types",
+                },
+                {
+                    "name": "International",
+                    "qcode": "w",
+                    "translations": {
+                        "name": {"en-CA": "International", "fr-CA": "International"}
                     },
-                    {
-                        "name": "Testing",
-                        "qcode": "149",
-                        "scheme": "onclusive_event_types",
+                },
+                {
+                    "name": "Business",
+                    "qcode": "b",
+                    "translations": {
+                        "name": {"en-CA": "Business", "fr-CA": "Affaires"}
                     },
-                    {
-                        "name": "Banking",
-                        "qcode": "159",
-                        "scheme": "onclusive_categories",
-                    },
-                    {
-                        "name": "Music Festivals",
-                        "qcode": "228",
-                        "scheme": "onclusive_event_types",
-                    },
-                    {
-                        "name": "Testing2",
-                        "qcode": "129",
-                        "scheme": "onclusive_event_types",
-                    },
-                    {
-                        "name": "Finance General",
-                        "qcode": "35",
-                        "scheme": "onclusive_categories",
-                    },
-                    {
-                        "name": "Tech - Internet, software & new media",
-                        "qcode": "50",
-                        "scheme": "onclusive_categories",
-                    },
-                    {
-                        "name": "Law & Order",
-                        "qcode": "88",
-                        "scheme": "onclusive_categories",
-                    },
-                    {
-                        "name": "Trade Conferences",
-                        "qcode": "97",
-                        "scheme": "onclusive_categories",
-                    },
-                    {
-                        "qcode": "Conference",
-                        "scheme": "event_types",
-                    },
-                    {
-                        "qcode": "Conference and trade show",
-                        "scheme": "event_types",
-                    },
-                    {
-                        "qcode": "Official visit",
-                        "scheme": "event_types",
-                    },
-                    {
-                        "qcode": "04000000",
-                        "scheme": "subject_custom",
-                    },
-                    {
-                        "qcode": "20000638",
-                        "scheme": "subject_custom",
-                    },
-                    {
-                        "qcode": "11000000",
-                        "scheme": "subject_custom",
-                    },
-                ]
-                self.assertEqual(
-                    sorted(map(qcode, item["subject"])),
-                    sorted(map(qcode, expected_subjects)),
-                )
-                item["anpa_category"].sort(key=lambda i: i["name"])
-                expected_categories = [
-                    {
-                        "name": "National",
-                        "qcode": "g",
-                        "translations": {
-                            "name": {
-                                "en-CA": "National",
-                                "fr-CA": "Nouvelles Générales",
-                            }
-                        },
-                    },
-                    {
-                        "name": "International",
-                        "qcode": "w",
-                        "translations": {
-                            "name": {"en-CA": "International", "fr-CA": "International"}
-                        },
-                    },
-                    {
-                        "name": "Business",
-                        "qcode": "b",
-                        "translations": {
-                            "name": {"en-CA": "Business", "fr-CA": "Affaires"}
-                        },
-                    },
-                ]
-                expected_categories.sort(key=lambda i: i["name"])
-                self.assertEqual(item["anpa_category"], expected_categories)
-                self.assertEqual(item[ITEM_TYPE], CONTENT_TYPE.EVENT)
-                self.assertEqual(item["state"], CONTENT_STATE.INGESTED)
+                },
+            ]
+            expected_categories.sort(key=lambda i: i["name"])
+            self.assertEqual(item["anpa_category"], expected_categories)
+            self.assertEqual(item[ITEM_TYPE], CONTENT_TYPE.EVENT)
+            self.assertEqual(item["state"], CONTENT_STATE.INGESTED)
 
-                self.assertEqual(item["occur_status"]["qcode"], "eocstat:eos5")
+            self.assertEqual(item["occur_status"]["qcode"], "eocstat:eos5")
 
-                self.assertIn(
-                    "https://www.canadianinstitute.com/anti-money-laundering-financial-crime/",
-                    item["links"],
-                )
+            self.assertIn(
+                "https://www.canadianinstitute.com/anti-money-laundering-financial-crime/",
+                item["links"],
+            )
 
-                self.assertEqual(
-                    item["dates"]["start"],
-                    datetime.datetime(2022, 6, 15, 0, 0, tzinfo=tzutc()),
-                )
-                self.assertEqual(
-                    item["dates"]["end"],
-                    datetime.datetime(2022, 6, 16, 0, 0, tzinfo=tzutc()),
-                )
+            self.assertEqual(
+                item["dates"]["start"],
+                datetime.datetime(2022, 6, 15, 0, 0, tzinfo=tzutc()),
+            )
+            self.assertEqual(
+                item["dates"]["end"],
+                datetime.datetime(2022, 6, 16, 0, 0, tzinfo=tzutc()),
+            )
 
-                self.assertEqual(
-                    item["name"],
-                    "Annual Forum on Anti-Money Laundering and Financial Crime",
-                )
-                self.assertEqual(item["definition_short"], "")
+            self.assertEqual(
+                item["name"],
+                "Annual Forum on Anti-Money Laundering and Financial Crime",
+            )
+            self.assertEqual(item["definition_short"], "")
 
-                self.assertEqual(
-                    item["location"][0]["name"],
-                    "One King West Hotel & Residence, 1 King St W, Toronto",
-                )
+            self.assertEqual(
+                item["location"][0]["name"],
+                "One King West Hotel & Residence, 1 King St W, Toronto",
+            )
+
+    def test_item_update(self):
+        # add a user
+        flask.g.user = {"_id": "current_user_id"}
+        event_service = get_resource_service("events")
+        provider = {
+            "_id": "abcd",
+            "source": "sf",
+            "name": "CP_Onclusive",
+        }
+        source = self.parser.parse(data)[0]
+
+        # Ingest first version
+        ingested, ids = ingest_item(source, provider=provider, feeding_service={})
+        self.assertTrue(ingested)
+        self.assertIn(source["guid"], ids)
+
+        dest = list(
+            event_service.get_from_mongo(req=None, lookup={"guid": source["guid"]})
+        )[0]
+        self.assertEqual(
+            dest["name"], "Annual Forum on Anti-Money Laundering and Financial Crime"
+        )
+        self.assertEqual(dest["state"], "ingested")
+        self.assertEqual(dest.get("version_creator"), None)
+
+        event_service.patch(
+            dest["_id"], {"name": "Edit event Name", "update_method": "single"}
+        )
+        dest = list(
+            event_service.get_from_mongo(req=None, lookup={"guid": source["guid"]})
+        )[0]
+        self.assertEqual(dest.get("version_creator"), "current_user_id")
+
+        # parser check version_creator, if it is there then cancel the update and return []
+        source = self.parser.parse(data)
+        self.assertEqual(source, [])
