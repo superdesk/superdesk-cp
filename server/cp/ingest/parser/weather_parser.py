20c8202084cc614bb64dfae8751a6e196c27a299
bzn-cp <142909055+bzn-cp@users.noreply.github.com>
2024-07-24 10:11:03 -0400

    Update weather_parser.py - Testing

--- server/cp/ingest/parser/weather_parser.py
+++ server/cp/ingest/parser/weather_parser.py
@@ -1,31 +1,24 @@
 from superdesk.io.feed_parsers.__init__ import FeedParser
-
 SOURCE = 'Weather Parser' 
 class WeatherParser(FeedParser):
     label = 'Weather Parser' 
     NAME = label.lower() # Unique name under which to register the class.
-
-
     def can_parse(self, article):
         # Assuming all files coming from FTP are valid
         return True
-
     def parse(self, article, provider=None):
         
         item = {}
         if self.can_parse(article):
             '''
             paragraphs = article.split('\n\n')
-
             # Extract the slugline and the description
             first_paragraph = paragraphs[0].split('\n')
             slugline = first_paragraph[0]
             description = '\n'.join(first_paragraph[1:])
-
             # Isolate the body 
             body = '\n\n'.join(paragraphs[1:])
             body = body.replace('\n','\\n') # Adjust for html formatting 
-
             
             # Populate item dictionary
             item['slugline'] = slugline
@@ -33,10 +26,10 @@ class WeatherParser(FeedParser):
             item['body_html'] = f"<p>{body}</p>"
             item["source"] =  'Environment Canada'
             '''
-            item['slugline'] = 'Test Slugline 1'
-            item['description_text'] =  'Test Description 1'
-            item['body_html'] =  'Test Body 1'
-            item["source"] =  'Environment Canada'
-            
+            item['slugline'] = 'Test Slugline1'
+            item['description_text'] =  'Test Description1'
+            item['body_html'] =  'Test Body1'
+            item["source"] =  'Environment Canada1'
+            item["guid"] =  '1234'
 
         return item 
