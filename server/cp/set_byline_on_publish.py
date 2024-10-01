5b4821da6593c405154862f0442efde780b7e829
Petr Jasek <jasekpetr@gmail.com>
2024-10-01 10:51:28 +0200

    fix author byline handling
    when used with author roles
    
    SDCP-772

--- server/cp/set_byline_on_publish.py
+++ server/cp/set_byline_on_publish.py
@@ -8,9 +8,13 @@ def set_byline_on_publish(sender, item, updates, **kwargs):
     if updated.get("byline") or not updated.get("authors"):
         return
 
-    byline = ", ".join([author["name"] for author in item.get("authors", [])])
+    byline = ", ".join([get_author_name(author) for author in item.get("authors", [])])
 
     item["byline"] = updates["byline"] = byline
+
+
+def get_author_name(author) -> str:
+    return author.get("sub_label") or author.get("name")
 
 
 def init_app(app):
