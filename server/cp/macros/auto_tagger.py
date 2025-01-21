8c2732cf682ed92e70a0e817afbe102cb6f43c7a
clockcode <clm@cp.org>
2025-01-21 14:13:00 -0500

    Enhance auto_tagger.py macro by adding ampersand replacement in headlines a - Introduced replace_ampersands function to ensure item headlines are formatted correctly.

--- server/cp/macros/auto_tagger.py
+++ server/cp/macros/auto_tagger.py
@@ -3,26 +3,40 @@ from flask import current_app as app
 from cp.ai.semaphore import Semaphore
 
 import logging
+
 logger = logging.getLogger(__name__)
+
 
 def qcode(value):
     return "{}:{}".format(value.get("scheme"), value.get("qcode"))
 
+
 def update_tag_relevance(existing_tag, new_tag):
     """Update the relevance of an existing tag with the new tag's relevance."""
-    if 'relevance' in new_tag and new_tag['relevance'] is not None:
-        existing_tag['relevance'] = new_tag['relevance']
+    if "relevance" in new_tag and new_tag["relevance"] is not None:
+        existing_tag["relevance"] = new_tag["relevance"]
+
+
+def replace_ampersands(item):
+    """Replace ampersands with 'and' in the item's content."""
+    if item.get("headline"):
+        item["headline"] = item["headline"].replace("&", "and")
+    return item
+
 
 def callback(item, **kwargs):
     """This macro will tag the item using the AutoTagging service based on the item's content."""
     semaphore = Semaphore(app)
+    item = replace_ampersands(item)
     tags = semaphore.analyze(item)
-    
+
     if tags:
         for key, values in tags.items():
             item.setdefault(key, [])
             for new_value in values:
-                existing_tag = next((tag for tag in item[key] if qcode(tag) == qcode(new_value)), None)
+                existing_tag = next(
+                    (tag for tag in item[key] if qcode(tag) == qcode(new_value)), None
+                )
                 if existing_tag:
                     update_tag_relevance(existing_tag, new_value)
                 else:
@@ -35,6 +49,3 @@ name = "auto-tagger"
 label = lazy_gettext("Auto Tagger")
 access_type = "backend"
 action_type = "direct"
-
-
-
