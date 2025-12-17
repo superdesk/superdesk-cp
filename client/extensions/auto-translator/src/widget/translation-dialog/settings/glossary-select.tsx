a3d8ee6a287614edc95744167e7150244c77a5dc
Taylor Li <t4ylor.li@gmail.com>
2025-12-17 11:06:58 -0500

    add select default glossary, style

--- client/extensions/auto-translator/src/widget/translation-dialog/settings/glossary-select.tsx
+++ client/extensions/auto-translator/src/widget/translation-dialog/settings/glossary-select.tsx
@@ -2,6 +2,7 @@ import { useFormikContext } from "formik";
 import * as React from "react";
 import { Option } from "superdesk-ui-framework/react";
 import { ErrorDialog, FormSelect } from "../../../components";
+import { typedSetFieldValue } from "../../../formik-utilties";
 import { superdesk } from "../../../superdesk";
 import { TranslationForm } from "../helpers";
 
@@ -20,9 +21,22 @@ export const GlossarySelect = () => {
   const { gettext } = superdesk.localization;
   const { httpRequestJsonLocal } = superdesk;
   const { showModal } = superdesk.ui;
-  const { values } = useFormikContext<TranslationForm>();
-
+  const { values, setFieldValue: formikSetFieldValue } =
+    useFormikContext<TranslationForm>();
+  const setFieldValue =
+    typedSetFieldValue<TranslationForm>(formikSetFieldValue);
   const [glossaries, setGlossaries] = React.useState<Array<Glossary>>([]);
+  const filteredGlossaries = React.useMemo<Array<Glossary>>(
+    () =>
+      glossaries.filter(({ dictionaries }) =>
+        dictionaries.some(
+          ({ source_lang, target_lang }) =>
+            source_lang === values.translateFrom &&
+            target_lang === values.translateTo
+        )
+      ),
+    [glossaries, values.translateFrom, values.translateTo]
+  );
 
   React.useEffect(() => {
     httpRequestJsonLocal<{ _items: Array<Glossary> }>({
@@ -43,25 +57,23 @@ export const GlossarySelect = () => {
       });
   }, []);
 
+  React.useEffect(() => {
+    const defaultGlossary = filteredGlossaries?.[0];
+    if (defaultGlossary) setFieldValue("glossary", defaultGlossary.glossary_id);
+    else setFieldValue("glossary", "");
+  }, [filteredGlossaries]);
+
   return (
     <FormSelect<TranslationForm> label={gettext("Glossary")} name={"glossary"}>
       <Option value="">{gettext("none")}</Option>
-      {glossaries
-        .filter(({ dictionaries }) =>
-          dictionaries.some(
-            ({ source_lang, target_lang }) =>
-              source_lang === values.translateFrom &&
-              target_lang === values.translateTo
-          )
-        )
-        .map((glossary) => (
-          <Option
-            value={glossary.glossary_id}
-            key={`glossary-${glossary.glossary_id}`}
-          >
-            {glossary.name}
-          </Option>
-        ))}
+      {filteredGlossaries.map((glossary) => (
+        <Option
+          value={glossary.glossary_id}
+          key={`glossary-${glossary.glossary_id}`}
+        >
+          {glossary.name}
+        </Option>
+      ))}
     </FormSelect>
   );
 };
--- client/extensions/auto-translator/src/widget/translation-dialog/settings/style-select.tsx
+++ client/extensions/auto-translator/src/widget/translation-dialog/settings/style-select.tsx
@@ -2,6 +2,7 @@ import { useFormikContext } from "formik";
 import * as React from "react";
 import { Option } from "superdesk-ui-framework/react";
 import { ErrorDialog, FormSelect } from "../../../components";
+import { typedSetFieldValue } from "../../../formik-utilties";
 import { superdesk } from "../../../superdesk";
 import { TranslationForm } from "../helpers";
 
@@ -15,9 +16,15 @@ export const StyleSelect = () => {
   const { gettext } = superdesk.localization;
   const { httpRequestJsonLocal } = superdesk;
   const { showModal } = superdesk.ui;
-  const { values } = useFormikContext<TranslationForm>();
-
+  const { values, setFieldValue: formikSetFieldValue } =
+    useFormikContext<TranslationForm>();
+  const setFieldValue =
+    typedSetFieldValue<TranslationForm>(formikSetFieldValue);
   const [styles, setStyles] = React.useState<Array<Style>>([]);
+  const filteredStyles = React.useMemo<Array<Style>>(
+    () => styles.filter(({ language }) => language === values.translateTo),
+    [styles, values.translateTo]
+  );
 
   React.useEffect(() => {
     httpRequestJsonLocal<{ _items: Array<Style> }>({
@@ -38,16 +45,20 @@ export const StyleSelect = () => {
       });
   }, []);
 
+  React.useEffect(() => {
+    const defaultStyle = filteredStyles?.[0];
+    if (defaultStyle) setFieldValue("style", defaultStyle.style_id);
+    else setFieldValue("style", "");
+  }, [filteredStyles]);
+
   return (
     <FormSelect<TranslationForm> label={gettext("Style")} name={"style"}>
       <Option value="">{gettext("none")}</Option>
-      {styles
-        .filter(({ language }) => language === values.translateTo)
-        .map((style) => (
-          <Option value={style.style_id} key={`style-${style.style_id}`}>
-            {style.name}
-          </Option>
-        ))}
+      {filteredStyles.map((style) => (
+        <Option value={style.style_id} key={`style-${style.style_id}`}>
+          {style.name}
+        </Option>
+      ))}
     </FormSelect>
   );
 };
